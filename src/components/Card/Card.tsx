c899089b41ad971aaae100b994f1526fe2c5fe8c
squallerQ <john199502031@gmail.com>
2025-07-26 12:41:41 +0200

    feat: convert Card to component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React, { Component, JSX } from 'react';
+import React, { JSX } from 'react';
 import styles from './Card.module.css';
 
 interface CardProps {
@@ -7,17 +7,14 @@ interface CardProps {
   imageUrl: string;
 }
 
-class Card extends Component<CardProps> {
-  render(): JSX.Element {
-    const { name, description, imageUrl } = this.props;
-    return (
-      <div className={styles.card}>
-        <img src={imageUrl} alt={name} className={styles.image} />
-        <h3 className={styles.title}>{name}</h3>
-        <p className={styles.description}>{description}</p>
-      </div>
-    );
-  }
+function Card({ name, description, imageUrl }: CardProps): JSX.Element {
+  return (
+    <div className={styles.card}>
+      <img src={imageUrl} alt={name} className={styles.image} />
+      <h3 className={styles.title}>{name}</h3>
+      <p className={styles.description}>{description}</p>
+    </div>
+  );
 }
 
 export default Card;
